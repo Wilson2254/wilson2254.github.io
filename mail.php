76d93ff9f66fe3f840b1840aabeba6419360efcb
Alexander_P <sanek191296@mail.ru>
2020-06-03 03:14:53 +0300

    Edit mail

--- mail.php
+++ mail.php
@@ -1,4 +1,4 @@
-<meta http-equiv='refresh' content='3; url=https://bloggood.ru'>
+<meta http-equiv='refresh' content='3; url=https://wilson2254.github.io/'>
 <meta charset="UTF-8" />
 <?php
 if (isset($_POST['name'])) {$name = $_POST['name']; if ($name == '') {unset($name);}}
@@ -12,7 +12,7 @@ $address = "sanek191296@mail.ru";
 $mes = "Имя: $name \nE-mail: $email \nТема: $sub \nТекст: $body";
 $send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
 if ($send == 'true')
-{echo "Сообщение отправлено успешно, через 3 секунд Вы будете направлены на главную страницу блога <a href='https://bloggood.ru/'>BLOGGOOD.RU</a>,где сможете продолжить ваш просмотр";}
+{echo "Сообщение отправлено успешно, через 3 секунд Вы будете направлены на главную страницу блога <a href='https://wilson2254.github.io/'>WebFace</a>,где сможете продолжить ваш просмотр";}
 else {echo "Ошибка, сообщение не отправлено!";}
 
 }
