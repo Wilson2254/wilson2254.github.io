ad4acd819b7df576bdf6b2e49973dc228ba89832
Alexander_P <sanek191296@mail.ru>
2020-06-14 21:19:51 +0300

    Create reverse function

--- works/Case-13/script.js
+++ works/Case-13/script.js
@@ -1,35 +1,13 @@
+function make_splice(){
+  let cell = document.getElementById('input_splice').value;
+  cell = cell.split('');
+  cell.splice(2, 3);
+  cell = cell.join('');
+  document.getElementById('res_splice').value = cell;
+}
 
-var f1, f2, zn;
-document.addEventListener('click', function(event) {
-  if (event.target.dataset.num != undefined) {
-    document.querySelector(".result>input").value+=event.target.value;
-  }
-  if (event.target.dataset.oper != undefined) {
-    if(event.target.value == "+" || event.target.value == "-" || event.target.value == "*" || event.target.value == "/")
-    {
-      window.zn = zn = event.target.value;
-      window.fl = f1 = Number(document.querySelector(".result>input").value);
-      document.querySelector(".result>input").value = '';
-    }
-    if(event.target.value == "=")
-    {
-      f2 = Number(document.querySelector(".result>input").value);
-      if (zn == "+") {
-        f2+=f1;
-        document.querySelector(".result>input").value = f2;
-      }
-      if (zn == "-") {
-        f1-=f2;
-        document.querySelector(".result>input").value = f1;
-      }
-      if (zn == "*") {
-        f1*=f2;
-        document.querySelector(".result>input").value = f1;
-      }
-      if (zn == "/") {
-        f1/=f2;
-        document.querySelector(".result>input").value = f1;
-      }
-    }
-  }
-});
+function make_reverse(){
+  let cell = document.getElementById('input_reverse').value;
+  cell = cell.split('').reverse().join('');
+  document.getElementById('res_reverse').value = cell;
+}
